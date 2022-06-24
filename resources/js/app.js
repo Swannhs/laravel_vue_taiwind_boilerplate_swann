47f80b446b6d76caf7232511c5acb5c5c1cc4116
Swann <princeapurbask@gmail.com>
2022-06-24 13:17:03 +0600

    completed boilearplate with laravel vue tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-require('./bootstrap');
+import store from "./store";
+import router from "./routes";
+import VueRouter from "vue-router";
+import index from './index';
+
+window.Vue = require('vue').default;
+
+Vue.use(VueRouter);
+
+const app = new Vue({
+    el: '#app',
+    router,
+    store,
+    components: {
+        index
+    }
+});
+
