0b120890d23bb65da53b810e4cf46096df911aa2
Swann <princeapurbask@gmail.com>
2022-06-26 10:11:30 +0600

    Vue layout

--- resources/js/index.vue
+++ resources/js/index.vue
@@ -1,15 +1,24 @@
 <template>
     <div>
-        <router-view></router-view>
+        <navbar></navbar>
+        <div class="container mx-auto vue-body">
+            <router-view></router-view>
+        </div>
+        <Footer></Footer>
     </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 export default {
-    name: "index"
+    name: "index",
+    components: {Footer, Navbar}
 }
 </script>
 
 <style scoped>
-
+.vue-body {
+    min-height: 55vh;
+}
 </style>
